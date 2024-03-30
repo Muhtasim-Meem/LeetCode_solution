c57e5238ef5fd4b42c94a74160afb9e462e098fb
Muhtasim <122847474+Muhtasim-Meem@users.noreply.github.com>
2024-03-30 16:24:35 +0600

    Add files via upload

---
+++ add_2_numbers.py
@@ -0,0 +1,14 @@
+l1=[9,9,9,9,9,9,9]
+l2=[9,9,9,9]
+l1.reverse()
+l2.reverse()
+string1 = ''.join(map(str, l1))
+string2 = ''.join(map(str, l2))
+sum_result = int(string1) + int(string2)
+x=str(sum_result)
+lst = [int(char) for char in x]
+lst.reverse()
+print(lst)
+print(sum_result)
+
+    
